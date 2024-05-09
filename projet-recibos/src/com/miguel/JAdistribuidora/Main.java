cb8dd2c29378f9fd03b7fec935b285ddde0bda46
miguel <miguel3.santos3@gmail.com>
2024-05-09 09:31:46 -0300

    Interação com o terminal

--- projet-recibos/src/com/miguel/JAdistribuidora/Main.java
+++ projet-recibos/src/com/miguel/JAdistribuidora/Main.java
@@ -3,14 +3,25 @@ package com.miguel.JAdistribuidora;
 import com.miguel.JAdistribuidora.model.Motorista;
 import com.miguel.JAdistribuidora.service.MotoristaService;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite nome do motorista: ");
+        String nome = scanner.next();
+
+        System.out.println("Digite o cpf do motorista: ");
+        String cpf = scanner.next();
+
+        System.out.println("Digite a chave pix do motorista: ");
+        Long chavePix = scanner.nextLong();
 
         Motorista motorista = new Motorista();
-        motorista.setNome("Miguel Maxmillian Martins dos Santos");
-        motorista.setCpf("123.456.789-10");
-        motorista.setChavePix(123456789L);
+        motorista.setNome(nome);
+        motorista.setCpf(cpf);
+        motorista.setChavePix(chavePix);
 
         MotoristaService motoristaService = new MotoristaService();
         motoristaService.cadastrar(motorista);
