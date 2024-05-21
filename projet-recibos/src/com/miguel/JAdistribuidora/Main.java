9f50f36721320c3e0e4d512c50cf85bfc71774d8
miguel <miguel3.santos3@gmail.com>
2024-05-21 19:25:12 -0300

    Criando a logica do gerador de recibos

--- projet-recibos/src/com/miguel/JAdistribuidora/Main.java
+++ projet-recibos/src/com/miguel/JAdistribuidora/Main.java
@@ -1,19 +1,25 @@
 package com.miguel.JAdistribuidora;
 
-import com.miguel.JAdistribuidora.model.Motorista;
 import com.miguel.JAdistribuidora.service.MotoristaService;
-
-import java.sql.SQLOutput;
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         MotoristaService motoristaService = new MotoristaService();
 
-        motoristaService.cadastrar("Miguel","(88) 99371-465", "090.844.183-52");
+        motoristaService.cadastrar("Miguel","(88) 9937-1465", "090.844.183-52");
         motoristaService.cadastrar("Sergio","(88) 8802-0122", "090.844.183-52");
 
         motoristaService.motoristasCadastrados();
+
+        System.out.println(" ");
+
+        motoristaService.remover("sergio");
+        motoristaService.motoristasCadastrados();
+
+        System.out.println(" ");
+
+        motoristaService.geradorRecibo("miguel");
+
     }
 }
--- projet-recibos/src/com/miguel/JAdistribuidora/service/MotoristaService.java
+++ projet-recibos/src/com/miguel/JAdistribuidora/service/MotoristaService.java
@@ -33,10 +33,70 @@ public class MotoristaService {
                 if (elemento.getNome().equalsIgnoreCase(nome)){
                     listRemover.add(elemento);
                 }
-                listMotoristas.removeAll(listRemover);
             }
+            listMotoristas.removeAll(listRemover);
         }
         else System.out.println("Lista vazia");
     }
 
+    public void geradorRecibo(String nome) {
+        /**
+         *  COMO PENSEI NA LOGICA:
+         *
+         *  Se o primeiro motorista analisado pelo "FOR" tiver o nome
+         *  igual ao nome do parametro que ta sendo recebio, gere um recibo, se nao vá
+         *  para o proximo.
+         *
+         *  Para  gerar o recibo selecione o motorista que tem o nome igual ao do parametro
+         *  adicione ele a um novo array e assim pegue o nome, depois pegue a chave pix e por ultimo pegue o cpf.
+         *
+         */
+
+
+
+        List<Motorista> listRecibos = new ArrayList<>();
+
+        for (Motorista motorista : listMotoristas){
+            if (motorista.getNome().equalsIgnoreCase(nome)){
+                listRecibos.add(motorista);
+            }
+            System.out.println("Eu " + listRecibos.get(0).getNome() + " de cpf " + listRecibos.get(0).getCpf() + " Recebi um valor de R$ 500,00, " +
+                    "que sera depositado nessa chave pix " + listRecibos.get(0).getChavePix());
+        }
+
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
